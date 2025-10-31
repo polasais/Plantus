17a902e947387e1a6916b4054fc2f6b7bbfcea15
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-10-31 23:56:51 +0200

    Call Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header';
 import { HomePage } from './pages/homepage/HomePage';
+import { Footer } from './components/Footer';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <>
             <Header />
             <HomePage />
+            <Footer />
         </>
     );
 }
